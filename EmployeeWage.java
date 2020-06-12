62bb280e322cd3ad13e540a8095f0703899e0238
unknown <skamil60@gmail.com>
2020-06-12 19:49:42 +0530

    Added : Monthly Wage Calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,11 +5,13 @@ class EmployeeWage
 		System.out.println("Welcome to the Employee wage program.");
 		int IS_PRESENT= 1;
 		int WAGE_PER_HOUR=20;
+		int WORKING_DAYS_PER_MONTH=20;
 		int FULL_DAY_HOUR=8;
 		int PART_TIME_HOUR=4;
 		int PART_TIME=2;
 		int FULL_TIME=1;
 		int DAILY_WAGE;
+		int MONTHLY_WAGE;
 		double empCheck=Math.floor(Math.random() *10)%3;
 		int randomCheck=(int)empCheck;
 
@@ -18,12 +20,14 @@ class EmployeeWage
 		case 1:
 
 			DAILY_WAGE=WAGE_PER_HOUR * FULL_DAY_HOUR;
-			System.out.println("Employee's Daily wage for full time is : " + DAILY_WAGE);
+			MONTHLY_WAGE=WORKING_DAYS_PER_MONTH * DAILY_WAGE;
+			System.out.println("Employee's Monthly wage for full time is : " + MONTHLY_WAGE);
 		break;
 		case 2:
 
 			DAILY_WAGE=WAGE_PER_HOUR * PART_TIME_HOUR;
-                        System.out.println("Employee's Daily wage for part time is : " + DAILY_WAGE);
+			MONTHLY_WAGE=WORKING_DAYS_PER_MONTH * DAILY_WAGE;
+                        System.out.println("Employee's Monthly wage for part time is : " + MONTHLY_WAGE);
 		break;
 		default:
 			System.out.println("Employee is absent.");
