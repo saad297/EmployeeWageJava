1f98d7e9957dca602ed6ec013daeb8848e1cec65
unknown <skamil60@gmail.com>
2020-06-12 14:39:11 +0530

    Added : Employee's Part Time Wage Calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,12 +6,21 @@ class EmployeeWage
 		int IS_PRESENT= 1;
 		int WAGE_PER_HOUR=20;
 		int FULL_DAY_HOUR=8;
+		int PART_TIME_HOUR=4;
+		int PART_TIME=2;
+		int FULL_TIME=1;
 		int DAILY_WAGE;
-		double empCheck=Math.floor(Math.random() *10)%2;
-		if(empCheck == IS_PRESENT)
+		double empCheck=Math.floor(Math.random() *10)%3;
+		System.out.println(empCheck);
+		if(empCheck == FULL_TIME)
 		{
 			DAILY_WAGE=WAGE_PER_HOUR * FULL_DAY_HOUR;
-			System.out.println("Employee's Daily wage is : " + DAILY_WAGE);
+			System.out.println("Employee's Daily wage for full time is : " + DAILY_WAGE);
+		}
+		else if (empCheck == PART_TIME)
+		{
+			DAILY_WAGE=WAGE_PER_HOUR * PART_TIME_HOUR;
+                        System.out.println("Employee's Daily wage for part time is : " + DAILY_WAGE);
 		}
 		else
 		{
